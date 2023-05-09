7b2ebd1dc244e59032a3ca3dbfe2b436ce1efb29
Andreaisgro01 <129078093+Andreaisgro01@users.noreply.github.com>
2023-05-09 16:45:00 +0200

    Add files via upload

--- pratica/script.js
+++ pratica/script.js
@@ -1,57 +1,35 @@
-//ESERCIZIO 1
 
-class user{
-    constructor(name,surname,age,location){
-        this.name = name;
-        this.surname = surname;
-        this.age = age;
-        this.location = location;
-    
-    }
+//input
+const nameField = document.querySelector('input');
+//buttons
+const saveButton = document.getElementById('saveButton');
+const deleteButton = document.getElementById('deleteButton');
+//paragraph
+const lastName = document.getElementById('target');
+//names array
+let names = [];
+
+//function save
+saveButton.addEventListener('click',save);
+
+function save(e){
+    e.preventDefault();
+    localStorage.setItem('Names', JSON.stringify(names));
+    names.push(nameField.value);
+    localStorage.setItem('Names', JSON.stringify(names));
+    lastName.textContent = names [names.length - 1];
+    document.getElementsByTagName('form').reset();
 }
 
-ageComparison = function () {
-    if(user1.age > user2.age){
-        return user1.name + ' è più vecchio di ' + user2.name
-    }  else if (user1.age < user2.age){
-        return user1.name +  ' è più giovane di '
-    + user2.name}
-        else {
-           return user1.name +' ha la stessa età di ' + user2.name
-        }
-    }
+//function delete
+deleteButton.addEventListener('click',cancel);
 
-let user1 = new user('Fabio','Cavilli','37','Milano');
-console.log(user1);
-
-let user2 = new user('Luca','Guerra','37','Chieti');
-console.log(user2);
-
-
-
-console.log(ageComparison());
-
-//ESERCIZIO 2
-
-let petNameInputfield = document.getElementById('pet-name');
-let ownerInputfield = document.getElementById('owner');
-let speciesInputfield = document.getElementById('species');
-let breedInputfield = document.getElementById('breed');
-
-class pet{
-    constructor(name,owner,species,breed){
-        this.name = name;
-        this.owner = owner;
-        this.species = species;
-        this.breed = breed;
-    }
-    static compareOwner = function(pet1,pet2){
-        return pet1.owner === pet2.owner;
-    }
-    
+function cancel(){
+    let names = JSON.parse(localStorage.getItem('Names'));
+    names.pop();
+    localStorage.setItems('Names',JSON.stringify(names));
+    lastName.textContent = names[names.length-1];
 }
-
-
 
 
 
